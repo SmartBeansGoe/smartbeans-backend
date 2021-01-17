5869ee1cbc6ac1d5a754d4069b4c029e9e2d3652
Niklas Birth <birth@posteo.de>
2021-01-17 23:22:44 +0100

    - Delete system messages after they have been received - Trigger "Verkleidungskünstler" achievement on login

--- src/system_messages.rs
+++ src/system_messages.rs
@@ -17,8 +17,15 @@ pub fn send_message(username: &str, message_type: &str, message_content: &str) {
 
 pub fn receive_messages(username: &str) -> Vec<SystemMessage> {
     use crate::schema::system_messages::dsl::*;
+    let conn = crate::database::establish_connection();
 
-    system_messages.filter(user.eq(username))
-        .load(&crate::database::establish_connection())
-        .expect("Database error")
+    let messages = system_messages.filter(user.eq(username))
+        .load(&conn)
+        .expect("Database error");
+
+    diesel::delete(system_messages.filter(user.eq(username)))
+        .execute(&conn)
+        .expect("Database error");
+
+    messages
 }
