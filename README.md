f47b1f67f1c7c076891fcb70894d77af2f5ff1b2
Niklas Birth <birth@posteo.de>
2021-08-31 15:16:29 +0200

    Added .gitignore, README and an initial Rocket Hello-World project

---
+++ README.md
@@ -0,0 +1,3 @@
+# smartbeans-backend
+
+https://pad.gwdg.de/Tj3wzg7iQNe8In47ltzelg?both
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![
+            index
+        ])
+        .launch()
+        .await
+        .unwrap();
 }
